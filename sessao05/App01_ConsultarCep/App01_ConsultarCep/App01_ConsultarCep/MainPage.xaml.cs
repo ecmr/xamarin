4eea4a7bae9e9ed9155ecc753690f40c60644a62
Edinei Correa de Menezes <edinei.menezes@gmail.com>
2020-04-14 22:21:37 -0300

    sessão5 - consulta cep básico funcionando

---
+++ sessao05/App01_ConsultarCep/App01_ConsultarCep/App01_ConsultarCep/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCep.Servico.Modelo;
+using App01_ConsultarCep.Servico;
+
+namespace App01_ConsultarCep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+            Endereco endereco = ViaCepServico.BuscarEnderecoViaCep(cep);
+            RESULTADO.Text = string.Format("Endereço: {0} , {1} , {2}/{3}", endereco.logradouro, endereco.bairro, endereco.localidade, endereco.uf);
+        }
+    }
+}
---
+++ sessao05/App01_ConsultarCep/App01_ConsultarCep/App01_ConsultarCep/Servico/ViaCepServico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCep.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCep.Servico
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoUrl = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoUrl = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+            string resultado = wc.DownloadString(NovoEnderecoUrl);
+
+            return JsonConvert.DeserializeObject<Endereco>(resultado);
+        }
+    }
+}
